718882c36140ee428073edfbdc941c8983e049bd
fjktkm <fjktkm@gmail.com>
2024-04-01 16:18:43 +0900

    fix: Println はフォーマット指定子が使えない

--- practice-a/go/main.go
+++ practice-a/go/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 func main() {
-    var a, b, c int
-    var s string
+	var a, b, c int
+	var s string
 
-    fmt.Scan(&a, &b, &c)
-    fmt.Scan(&s)
-    fmt.Println("%d %s", a+b+c, s)
+	fmt.Scan(&a, &b, &c)
+	fmt.Scan(&s)
+	fmt.Println(a+b+c, s)
 }
